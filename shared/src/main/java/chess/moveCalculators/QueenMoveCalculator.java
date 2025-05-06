8b99dba20bd1d87db79c4c41114b10d309fc4520
laurenellington <laurenellington06@gmail.com>
2025-05-06 17:14:06 -0600

    Implement QueenMoveCalculator

--- shared/src/main/java/chess/moveCalculators/QueenMoveCalculator.java
+++ shared/src/main/java/chess/moveCalculators/QueenMoveCalculator.java
@@ -8,6 +8,28 @@ import java.util.Collection;
 public class QueenMoveCalculator implements PieceMoveCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+        int[][] direction = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
+        for(int i = 0; i < direction.length;i++){
+            int row_modifier = direction[i][0];
+            int col_modifier = direction[i][1];
+            int row = myPosition.getRow()+row_modifier;
+            int col = myPosition.getColumn()+col_modifier;
+            while(row >= 1 && row <= 8 && col >=1 && col <= 8){
+                ChessPosition newPosition = new ChessPosition(row,col);
+                if(board.getPiece(newPosition)==null){//if there is no piece in the potential move location
+                    moves.add(new ChessMove(myPosition,newPosition,null));
+                }
+                else{
+                    if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(myPosition).getTeamColor()){//if the piece is not of my color
+                        moves.add(new ChessMove(myPosition,newPosition,null));//then i can potentially take it
+                    }
+                    break;
+                }
+                row+=row_modifier;
+                col+=col_modifier;
+            }
+        }
+
         return moves;
     }
 }
