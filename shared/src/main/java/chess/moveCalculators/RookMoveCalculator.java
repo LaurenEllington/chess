12b2d6614eb2d79c17fab9a3f4648677d067846b
laurenellington <laurenellington06@gmail.com>
2025-05-06 16:44:27 -0600

    Simplified RookMoveCalculator code

--- shared/src/main/java/chess/moveCalculators/RookMoveCalculator.java
+++ shared/src/main/java/chess/moveCalculators/RookMoveCalculator.java
@@ -9,54 +9,25 @@ import java.util.ArrayList;
 public class RookMoveCalculator implements PieceMoveCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
-        //i hate this code
-        for(int col = myPosition.getColumn()-1;col>0;col--){//everything on same row as rook to the left
-            ChessPosition newPosition = new ChessPosition(myPosition.getRow(),col);
-            if(board.getPiece(newPosition)==null){//if there is no piece in the potential move location
-                moves.add(new ChessMove(myPosition,newPosition,null));
-            }
-            else{
-                if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(myPosition).getTeamColor()){//if the piece is not of my color
-                    moves.add(new ChessMove(myPosition,newPosition,null));//then i can potentially take it
+        int[][] direction = {{1,0},{-1,0},{0,1},{0,-1}};
+        for(int i = 0; i < direction.length;i++){
+            int row_modifier = direction[i][0];
+            int col_modifier = direction[i][1];
+            int row = myPosition.getRow()+row_modifier;
+            int col = myPosition.getColumn()+col_modifier;
+            while(row >= 1 && row <= 8 && col >=1 && col <= 8){
+                ChessPosition newPosition = new ChessPosition(row,col);
+                if(board.getPiece(newPosition)==null){//if there is no piece in the potential move location
+                    moves.add(new ChessMove(myPosition,newPosition,null));
                 }
-                break;
-            }
-        }
-        for(int col = myPosition.getColumn()+1;col<=8;col++){//everything on same row as rook to the right
-            ChessPosition newPosition = new ChessPosition(myPosition.getRow(),col);
-            if(board.getPiece(newPosition)==null){//if there is no piece in the potential move location
-                moves.add(new ChessMove(myPosition,newPosition,null));
-            }
-            else{
-                if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(myPosition).getTeamColor()){//if the piece is not of my color
-                    moves.add(new ChessMove(myPosition,newPosition,null));//then i can potentially take it
+                else{
+                    if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(myPosition).getTeamColor()){//if the piece is not of my color
+                        moves.add(new ChessMove(myPosition,newPosition,null));//then i can potentially take it
+                    }
+                    break;
                 }
-                break;
-            }
-        }
-        //up/down
-        for(int row = myPosition.getRow()-1;row>0;row--){//everything on same row as rook to the left
-            ChessPosition newPosition = new ChessPosition(row,myPosition.getColumn());
-            if(board.getPiece(newPosition)==null){//if there is no piece in the potential move location
-                moves.add(new ChessMove(myPosition,newPosition,null));
-            }
-            else{
-                if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(myPosition).getTeamColor()){//if the piece is not of my color
-                    moves.add(new ChessMove(myPosition,newPosition,null));//then i can potentially take it
-                }
-                break;
-            }
-        }
-        for(int row = myPosition.getRow()+1;row<=8;row++){//everything on same row as rook to the right
-            ChessPosition newPosition = new ChessPosition(row,myPosition.getColumn());
-            if(board.getPiece(newPosition)==null){//if there is no piece in the potential move location
-                moves.add(new ChessMove(myPosition,newPosition,null));
-            }
-            else{
-                if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(myPosition).getTeamColor()){//if the piece is not of my color
-                    moves.add(new ChessMove(myPosition,newPosition,null));//then i can potentially take it
-                }
-                break;
+                row+=row_modifier;
+                col+=col_modifier;
             }
         }
 
