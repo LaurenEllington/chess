43c271014c45f3ec751014e6bfc85187f3f57d88
laurenellington <laurenellington06@gmail.com>
2025-05-06 22:13:24 -0600

    Add directionMovement class to simplify code for the pieces that repeatedly move in the same direction aka queen, rook, bishop Change BishopMoveCalculator, QueenMoveCalculator, RookMoveCalculator to incorporate directionMovement to decrease repetitive code

--- shared/src/main/java/chess/movecalculators/RookMoveCalculator.java
+++ shared/src/main/java/chess/movecalculators/RookMoveCalculator.java
@@ -8,30 +8,9 @@ import java.util.ArrayList;
 
 public class RookMoveCalculator implements PieceMoveCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
         int[][] direction = {{1,0},{-1,0},{0,1},{0,-1}};
-        for(int i = 0; i < direction.length;i++){
-            int rowModifier = direction[i][0];
-            int colModifier = direction[i][1];
-            int row = myPosition.getRow()+rowModifier;
-            int col = myPosition.getColumn()+colModifier;
-            while(row >= 1 && row <= 8 && col >=1 && col <= 8){
-                ChessPosition newPosition = new ChessPosition(row,col);
-                if(board.getPiece(newPosition)==null){//if there is no piece in the potential move location
-                    moves.add(new ChessMove(myPosition,newPosition,null));
-                }
-                else{
-                    if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(myPosition).getTeamColor()){//if the piece is not of my color
-                        moves.add(new ChessMove(myPosition,newPosition,null));//then i can potentially take it
-                    }
-                    break;
-                }
-                row+= rowModifier;
-                col+= colModifier;
-            }
-        }
-
-        return moves;
+        directionMovement moves = new directionMovement();
+        return moves.pieceMoves(board,myPosition,direction);
     }
 
 }
