3d309bc29f5d61e921a70d463b2912da8db634a6
laurenellington <laurenellington06@gmail.com>
2025-05-22 19:22:24 -0600

    Add files Add dependencies

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,28 @@
+package server;
+
+import chess.*;
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+        // Register your endpoints and handle exceptions here.
+        Spark.delete("/db",(req, res) -> {
+            res.status(200);
+            return "{}";
+        });
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        //Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
