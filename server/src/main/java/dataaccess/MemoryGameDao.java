7983be15c056814cf48d6207b5f074b6273610f3
laurenellington <laurenellington06@gmail.com>
2025-05-23 22:25:57 -0600

    Create MemoryGameDao to store GameData in local memory

---
+++ server/src/main/java/dataaccess/MemoryGameDao.java
@@ -0,0 +1,40 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class MemoryGameDao {
+    private static ArrayList<GameData> games = new ArrayList<GameData>();
+    //not sure if games should be static yet
+    public void createGame(GameData game) throws DataAccessException{
+        games.add(game);
+    }
+    public GameData getGame(int gameID) throws DataAccessException{
+        for(GameData game : games){
+            if(game.gameID() == gameID){
+                return game;
+            }
+        }
+        return null;
+    }
+    public Collection<GameData> listGames() throws DataAccessException{
+        return games;
+        //note: GameData toString method is not overriden so this will be ugly
+    }
+    public void updateGame(int gameID, GameData game) throws DataAccessException{
+        for(int i = 0; i < games.size(); i++){
+            if(games.get(i).gameID() == gameID){
+                games.set(i,game);
+                break;
+            }
+        }
+    }
+    public void deleteGame(GameData game) throws DataAccessException{
+        games.remove(game);
+    }
+    public void clearGameData(){
+        games.clear();
+    }
+}
