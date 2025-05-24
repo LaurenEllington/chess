5685314b3405f4793e776bf62470fb957f514032
laurenellington <laurenellington06@gmail.com>
2025-05-24 03:47:13 -0600

    Create handler classes

---
+++ server/src/main/java/handler/LogoutHandler.java
@@ -0,0 +1,8 @@
+package handler;
+
+public class LogoutHandler {
+    //take info out of json and authtoken and turn into logoutrequest object
+    //pass data to service
+    //call userservice logout method
+    //turn result object into json string
+}
