2be6bd29cdc67d5b0475473f24152d6b61ec3bb6
laurenellington <laurenellington06@gmail.com>
2025-06-05 21:57:24 -0600

    Add tests for MySqlUserDao getUser(username), getUser(username,password) Move code into addValidUser and compareUser methods to reduce code duplication

--- server/src/test/java/dataaccess/DatabaseUnitTests.java
+++ server/src/test/java/dataaccess/DatabaseUnitTests.java
@@ -24,6 +24,8 @@ public class DatabaseUnitTests {
     private static final MySqlAuthDao authDao = new MySqlAuthDao();
     private static final MySqlGameDao gameDao = new MySqlGameDao();
     private static final MySqlUserDao userDao = new MySqlUserDao();
+    public static final String insertError = "Error: unable to insert into database: %s";
+    public static final String getError = "Error: could not get %s from database";
 
     //should either recreate tables or end transaction
     @BeforeEach
@@ -37,7 +39,7 @@ public class DatabaseUnitTests {
     @Test
     public void testAddUser() throws DataAccessException{
         Assertions.assertDoesNotThrow( () -> userDao.createUser(testUser),
-                String.format("Error: unable to insert into database: %s", testUser));
+                String.format(insertError, testUser));
         var sql = "SELECT username, password, email FROM user WHERE username=?";
         try (var conn = DatabaseManager.getConnection()){
             try (PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -47,13 +49,7 @@ public class DatabaseUnitTests {
                 String dbUsername = rs.getString(1);
                 String dbPassword = rs.getString(2);
                 String dbEmail = rs.getString(3);
-                String errorMessage = "Error: %s added into database is different than username provided";
-                Assertions.assertEquals(dbUsername,testUser.username(), String.format(errorMessage,"username"));
-                Assertions.assertNotEquals(dbPassword,testUser.password(), "Error: password not encrypted");
-                Assertions.assertTrue(() -> BCrypt.checkpw(testUser.password(),dbPassword),
-                        String.format(errorMessage,"password"));
-                Assertions.assertEquals(dbEmail,testUser.email(),String.format(errorMessage,"email"));
-
+                compareUser(dbUsername,dbPassword,dbEmail);
             }
         } catch (SQLException ex){
             throw new DataAccessException("Error: " + ex.getMessage());
@@ -61,8 +57,8 @@ public class DatabaseUnitTests {
 
     }
     @Test
-    public void testInvalidUser(){
-        userDao.createUser(testUser);
+    public void testInvalidUser() throws DataAccessException{
+        addValidUser();
         boolean throwed = false;
         try {
             userDao.createUser(testUser);
@@ -71,12 +67,59 @@ public class DatabaseUnitTests {
         }
         Assertions.assertTrue(throwed,
                 "Error: adding a duplicate user to the database did not throw an exception");
-
+    }
+    @Test
+    public void testGetUser() throws DataAccessException{
+        addValidUser();
+        UserData dbUser = Assertions.assertDoesNotThrow(() ->
+                userDao.getUser(testUser.username()),String.format(getError,testUser));
+        compareUser(dbUser.username(),dbUser.password(),dbUser.email());
+    }
+    @Test
+    public void testGetNonexistentUser(){
+        Assertions.assertNull(userDao.getUser(testUser.username()),
+                "Error: calling getUser on a nonexistent user did not return null");
+    }
+    @Test
+    public void testRightPassword() throws DataAccessException{
+        addValidUser();
+        UserData dbUser = Assertions.assertDoesNotThrow(() ->
+                userDao.getUser(testUser.username(),testUser.password()),
+                String.format(getError,testUser));
+        compareUser(dbUser.username(),dbUser.password(),dbUser.email());
+    }
+    @Test
+    public void testWrongPassword() throws DataAccessException{
+        addValidUser();
+        Assertions.assertNull(userDao.getUser(testUser.username(),"invalid"),
+                "Error: retrieved user with a password that was incorrect");
     }
     @Test
     public void testCreateGame() {
         Assertions.assertDoesNotThrow( () -> gameDao.createGame(testGame));
 
         var statement = "SELECT username, password, email FROM user WHERE username=?";
+    }
+    private void addValidUser() throws DataAccessException{
+        String hash = BCrypt.hashpw(testUser.password(),BCrypt.gensalt());
+        var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection()){
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1,testUser.username());
+                ps.setString(2,hash);
+                ps.setString(3,testUser.email());
+                ps.executeUpdate();
+            }
+        } catch (SQLException ex){
+            throw new DataAccessException("Error: could not access database " + ex.getMessage());
+        }
+    }
+    private void compareUser(String dbUsername, String dbPassword, String dbEmail){
+        String errorMessage = "Error: %s added into database is different than username provided";
+        Assertions.assertEquals(dbUsername,testUser.username(), String.format(errorMessage,"username"));
+        Assertions.assertNotEquals(dbPassword,testUser.password(), "Error: password not encrypted");
+        Assertions.assertTrue(() -> BCrypt.checkpw(testUser.password(),dbPassword),
+                String.format(errorMessage,"password"));
+        Assertions.assertEquals(dbEmail,testUser.email(),String.format(errorMessage,"email"));
     }
 }
